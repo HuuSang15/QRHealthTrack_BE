2d2558ddb4c5839f83c9f7c2b023ac43fd30ea87
HuuSang15 <phamhuusang24@gmail.com>
2024-01-01 22:08:55 +0700

    Update config

--- configs/configuration.js
+++ configs/configuration.js
@@ -3,7 +3,7 @@ module.exports = {
     "mongodb+srv://phamhuusang24:Phamhuusang_247@cluster0.lrmx9h0.mongodb.net/?retryWrites=true&w=majority",
   ADMIN: {
     username: "admin",
-    password: "1",
+    password: "admin",
     role: 1,
     fullName: "admin",
   },
